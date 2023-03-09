0f088e51727ec713b2d40757faa68c3a2f6929d2
tutorcs <tutorcs@163.com>
2023-03-09 09:45:21 +0800

    WeChat cstutorcs CSE224 project 5

---
+++ pkg/surfstore/RaftConstants.go
@@ -0,0 +1,12 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+package surfstore
+
+import (
+	"fmt"
+)
+
+var ERR_SERVER_CRASHED = fmt.Errorf("Server is crashed.")
+var ERR_NOT_LEADER = fmt.Errorf("Server is not the leader")
---
+++ pkg/surfstore/RaftInterfaces.go
@@ -0,0 +1,28 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+package surfstore
+
+import (
+	context "context"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
+)
+
+type RaftInterface interface {
+	AppendEntries(ctx context.Context, input *AppendEntryInput) (*AppendEntryOutput, error)
+	SetLeader(ctx context.Context, _ *emptypb.Empty) (*Success, error)
+	SendHeartbeat(ctx context.Context, _ *emptypb.Empty) (*Success, error)
+}
+
+type RaftTestingInterface interface {
+	GetInternalState(ctx context.Context, _ *emptypb.Empty) (*RaftInternalState, error)
+	Crash(ctx context.Context, _ *emptypb.Empty) (*Success, error)
+	Restore(ctx context.Context, _ *emptypb.Empty) (*Success, error)
+}
+
+type RaftSurfstoreInterface interface {
+	MetaStoreInterface
+	RaftInterface
+	RaftTestingInterface
+}
